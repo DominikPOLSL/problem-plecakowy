622be3f4e26d36cb2ad1d7e49a7833ddc9b8e103
Dominik Pindych <dp300860@student.polsl.pl>
2024-05-13 23:26:16 +0200

    weight repeatability removed

--- bag.py
+++ bag.py
@@ -3,6 +3,7 @@ class Bag:
     def __init__(self):
         self.max = 100
         self.current = 0
+        self.value = 0
         self.lista = [{}]
 
     def wypisz(self):
@@ -11,21 +12,32 @@ class Bag:
     def dodaj(self, dic: {}):
         self.lista.append(dic)
         self.current += dic.get("Weight")
-        print(self.current)
+        self.value += dic.get("Value")
+        #print("Dodano", dic.get("Weight"), "Aktualnie: ",self.current)
 
-    def CanAdd(self, dic: {},):
+    def check(self, used_item: [{}]):
+        if self.current > self.max:
+            #print("Powyzej wagi")
+            return False
 
-        finall = True
+        if used_item in self.lista:
+            #print("Ten sam item")
+            return False
 
-        if self.current + dic.get("Weight") > self.max:
-            finall = False
-
-
-        for i in self.lista:
-            if dic.get("Name") == i.get("Name"):
-                finall = False
-
-        return finall
+        else: return True
+    # def CanAdd(self, dic: {},):
+    #
+    #     finall = True
+    #
+    #     if self.current + dic.get("Weight") > self.max:
+    #         finall = False
+    #
+    #
+    #     for i in self.lista:
+    #         if dic.get("Name") == i.get("Name"):
+    #             finall = False
+    #
+    #     return finall
 
 
 
